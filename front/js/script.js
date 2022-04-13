493cef7a6ae7bd5080c30db62fb9fb8ce5a6e33a
Ceylu <87119259+CelineSebe@users.noreply.github.com>
2022-04-13 16:57:25 +0200

    requete API GET

---
+++ front/js/script.js
@@ -0,0 +1,31 @@
+fetch("http://localhost:3000/api/products")
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then((products) => {
+    products.forEach((kanap) => {
+      const items = document.querySelector(".items");
+      const a = document.createElement("a");
+      const article = document.createElement("article");
+      const image = document.createElement("img");
+      const h3 = document.createElement("h3");
+      const p = document.createElement("p");
+
+      h3.classList.add("productName");
+      p.classList.add("productDescription");
+
+      image.src = kanap.imageUrl;
+      image.alt = kanap.altTxt;
+      h3.innerText = kanap.name;
+      p.innerText = kanap.description;
+      a.href = "product.html?id=" + kanap._id;
+
+      items.appendChild(a);
+      a.appendChild(article);
+      article.appendChild(image);
+      article.appendChild(h3);
+      article.appendChild(p);
+    });
+  });
