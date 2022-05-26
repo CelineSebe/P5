3c49283e0dd39604e59af90132cec214f6cf713b
Céline Sèbe <celine.sebe@gmail.com>
2022-05-26 12:20:57 +0200

    function deleteProduct

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,5 @@
 // Récupération du panier de produit LS
-const productInLocalStorage = JSON.parse(localStorage.getItem("panier"));
+let productInLocalStorage = JSON.parse(localStorage.getItem("panier"));
 //Mise en place de l'Api en lien avec l'id du produit sélectionné
 
  fetch('http://localhost:3000/api/products/')
@@ -11,6 +11,7 @@ const productInLocalStorage = JSON.parse(localStorage.getItem("panier"));
      //2ème promesse: obtenir l'objet JS
     .then(function createBasket() {
         getBasket();
+        deleteProduct();
     })    
     // Si l'API ne répond pas
     // .catch(function(err) {
@@ -68,43 +69,66 @@ function getBasket(){
       }
     }
 getBasket();
+/***************** ------------ Supprimer l'article ---------************************************************/
+deleteProduct();
 
-/***************** ------------ Fonction pour mettre à jour les quantités ----------- ***********************/
-function changeQuantity() {
-  const positionProduct = document.getElementById("cart__items");
-  // manière de regarder ce que l'on a d'affiché dynamiquement grace au dataset
-  // On écoute ce qu'il se passe dans itemQuantity de l'article concerné
-  positionProduct.forEach((positionProduct) => {
-    positionProduct.addEventListener("change", (eq) => {
-      // vérification d'information de la valeur du clic et son positionnement dans les articles
-      
-          totalProduit();
-        });
-    });
-  };
+function deleteProduct(){
+const deleteProd = document.querySelectorAll(".deleteItem");
 
-
-/*****-------- fonction ajout nombre total produit et coût total ------*********/
-
-function totalProduit() {
+// selection de l'ID supprimé en cliquant sur le bouton
+for (let i =0; i < deleteProd.length; i++ ){
+deleteProd[i].addEventListener("click",(e) => {
+  e.preventDefault();
   let productInLocalStorage = JSON.parse(localStorage.getItem("panier"));
-  // déclaration variable en tant que nombre
-  let totalArticle = 0;
-  // déclaration variable en tant que nombre
-  let prixCombiné = 0;
-  // déclaration variable en tant que nombre
-  let totalPrix = 0;
-  // j'ajoute toutes les quantités d'article du panier et calcule la somme/prix total
-  for (let article of productInLocalStorage) {
-    totalArticle += JSON.parse(article.quantité);
-    prixCombiné = JSON.parse(article.quantité) * JSON.parse(article.prix);
-    totalPrix += prixCombiné;
-  }
-  // je pointe l'endroit d'affichage nombre d'article
-  document.getElementById("totalQuantity").textContent = totalArticle;
-  // je pointe l'endroit d'affichage du prix total
-  document.getElementById("totalPrice").textContent = totalPrix;
+  let id_delete = productInLocalStorage[i]._id;
+
+  //methode slice pour extraire une chaîne de caractère et la retourne comme une nouvelle chaîne de caractères
+  productInLocalStorage = productInLocalStorage.filter(element => element._id !== id_delete);
+  console.log(productInLocalStorage);
+  // envoi de la variable LS
+  localStorage.setItem("panier", JSON.stringify(productInLocalStorage));
+  alert("Ce produit vient d'être supprimé")
+  window.location.href = "cart.html";
+  })
 }
+
+};
+// /***************** ------------ Fonction pour mettre à jour les quantités ----------- ***********************/
+// function changeQuantity() {
+//   const positionProduct = document.getElementById("cart__items");
+//   // manière de regarder ce que l'on a d'affiché dynamiquement grace au dataset
+//   // On écoute ce qu'il se passe dans itemQuantity de l'article concerné
+//   positionProduct.forEach((positionProduct) => {
+//     positionProduct.addEventListener("change", (eq) => {
+//       // vérification d'information de la valeur du clic et son positionnement dans les articles
+      
+//           totalProduit();
+//         });
+//     });
+//   };
+
+
+// /*****-------- fonction ajout nombre total produit et coût total ------*********/
+
+// function totalProduit() {
+//   let productInLocalStorage = JSON.parse(localStorage.getItem("panier"));
+//   // déclaration variable en tant que nombre
+//   let totalArticle = 0;
+//   // déclaration variable en tant que nombre
+//   let prixCombiné = 0;
+//   // déclaration variable en tant que nombre
+//   let totalPrix = 0;
+//   // j'ajoute toutes les quantités d'article du panier et calcule la somme/prix total
+//   for (let article of productInLocalStorage) {
+//     totalArticle += JSON.parse(article.quantité);
+//     prixCombiné = JSON.parse(article.quantité) * JSON.parse(article.prix);
+//     totalPrix += prixCombiné;
+//   }
+//   // je pointe l'endroit d'affichage nombre d'article
+//   document.getElementById("totalQuantity").textContent = totalArticle;
+//   // je pointe l'endroit d'affichage du prix total
+//   document.getElementById("totalPrice").textContent = totalPrix;
+// }
 // Ecoute la validation de la commande lors de l'envoi du formulaire
 document.getElementById("order").addEventListener("click",function(){
     //champs à compléter
